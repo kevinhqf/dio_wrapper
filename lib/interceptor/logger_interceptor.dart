d2b6c29c3e34ef78bf4ece364e201f412d8437c7
kevin_ho <kevin.ho.hqf@gmail.com>
2022-07-04 10:36:50 +0800

    init

--- lib/interceptor/logger_interceptor.dart
+++ lib/interceptor/logger_interceptor.dart
@@ -10,7 +10,7 @@ class LoggerInterceptor extends InterceptorsWrapper {
         errorMethodCount: 8, // number of method calls if stacktrace is provided
         lineLength: 800, // width of the output
         colors: true, // Colorful log messages
-        printEmojis: false, // Print an emoji for each log message
+        printEmojis: true, // Print an emoji for each log message
         printTime: true // Should each log print contain a timestamp
     ),
   );
